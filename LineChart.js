adac15ce2ad537eb9c27ee0c25ec250e5142dbd6
RajniPal <rajnipal748@gmail.com>
2016-12-27 14:41:13 +0530

    Add files via upload

---
+++ LineChart.js
@@ -0,0 +1,25 @@
+﻿AngualrsJSApp.controller('LineController', function LineController($scope) {
+    $scope.Welcome ="Hi, Welcome to Machine Test";
+    $scope.chart = new CanvasJS.Chart("chartContainer",
+        {
+            theme: "theme2",
+            backgroundColor: "yellow",
+            zoomEnabled: true,
+            title: { text: "Quantity of Fruits" },
+            axisY:{
+                title : "In Kg",
+                 },
+            data:[{
+                type: "line",
+            dataPoints:[
+                        { label: "apple", y: 450 },
+                        { label: "orange", y: 414 },
+                        { label: "orange", y: 420 },
+                        { label: "mango", y: 460 },
+                        { label: "grape", y: 500 },
+            ]
+        }]
+     });
+    $scope.chart.render();
+
+});
